af61522e29f5a84057b350f2d5b54d1ac4db9e06
tarun <tarunonmail91@gmail.com>
2021-07-29 22:59:50 -0400

    generalized the search word

--- scraper/bbscraper.py
+++ scraper/bbscraper.py
@@ -9,42 +9,46 @@ import os
 from time import sleep
 
 
-useragent = fake_useragent.UserAgent()
-opts = Options()
-opts.add_argument(f"-user-agent={useragent.random}")
-# profile = webdriver.FirefoxProfile()
-# profile.set_preference("general.useragent.override", useragent.random)
-# profile.set_preference("dom.popup_maximum", 0)
-# profile.set_preference("webdriver.load.strategy", "unstable")
-# print(useragent.random)
-# # options.add_argument("-devtools")
+def best_buy_search(search_word):
+    useragent = fake_useragent.UserAgent()
+    opts = Options()
+    opts.add_argument(f"-user-agent={useragent.random}")
+    # profile = webdriver.FirefoxProfile()
+    # profile.set_preference("general.useragent.override", useragent.random)
+    # profile.set_preference("dom.popup_maximum", 0)
+    # profile.set_preference("webdriver.load.strategy", "unstable")
+    # print(useragent.random)
+    # # options.add_argument("-devtools")
 
-# driver = webdriver.Firefox(firefox_options=options, firefox_profile=profile)
-driver = webdriver.Chrome(options=opts, service_log_path=os.devnull)
-wait = WebDriverWait(driver, 20)
-driver.get("http://www.bestbuy.com")
+    # driver = webdriver.Firefox(firefox_options=options, firefox_profile=profile)
+    driver = webdriver.Chrome(options=opts, service_log_path=os.devnull)
+    wait = WebDriverWait(driver, 20)
+    driver.get("http://www.bestbuy.com")
 
-element = wait.until(
-    EC.element_to_be_clickable(
-        (
-            By.XPATH,
-            '//*[@id="widgets-view-email-modal-mount"]/div/div/div[1]/div/div/div/div/button',
+    element = wait.until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                '//*[@id="widgets-view-email-modal-mount"]/div/div/div[1]/div/div/div/div/button',
+            )
         )
     )
-)
-element.click()
+    element.click()
 
-element = wait.until(
-    EC.visibility_of_element_located((By.CSS_SELECTOR, "#gh-search-input"))
-)
-sleep(5)
-element.send_keys("PS5 Console")
-(
-    driver.find_element_by_xpath(
-        "/html/body/div[2]/div/div/div[1]/header/div[1]/div/div[1]/div/form/button[2]"
+    element = wait.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#gh-search-input"))
     )
-).click()
+    sleep(5)
+    element.send_keys(search_word)
+    (
+        driver.find_element_by_xpath(
+            "/html/body/div[2]/div/div/div[1]/header/div[1]/div/div[1]/div/form/button[2]"
+        )
+    ).click()
 
-sleep(30)
-driver.close()
-driver.quit()
+    sleep(30)
+    driver.close()
+    driver.quit()
+
+
+best_buy_search(search_word="PS5 Console")
