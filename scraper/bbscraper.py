5af37d9a9cbf8fccb7c76564323c872d6e671e04
tarun <tarunonmail91@gmail.com>
2021-08-01 15:04:19 -0400

    "bestbuy module ready, can be used with -p flag to search for a product"

--- scraper/bbscraper.py
+++ scraper/bbscraper.py
@@ -5,13 +5,20 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import fake_useragent
 import os
+import argparse
 from time import sleep
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--product", help="Product Name")
+args = parser.parse_args()
 
 
 def best_buy_search(search_word):
     useragent = fake_useragent.UserAgent()
     opts = Options()
-    opts.add_argument(f"-user-agent={useragent.random}")
+    opts.add_argument(
+        f"-user-agent={'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}"
+    )
     driver = webdriver.Chrome(options=opts, service_log_path=os.devnull)
     wait = WebDriverWait(driver, 20)
     driver.get("http://www.bestbuy.com")
@@ -32,14 +39,41 @@ def best_buy_search(search_word):
     sleep(5)
     element.send_keys(search_word)
     (
-        driver.find_element_by_xpath(
-            "/html/body/div[2]/div/div/div[1]/header/div[1]/div/div[1]/div/form/button[2]"
-        )
+        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "header-search-button")))
     ).click()
-
-    sleep(30)
-    driver.close()
+    product_list = driver.find_element_by_class_name("sku-item-list")
+    items = product_list.find_elements_by_class_name("sku-item")
+    for item in items:
+        try:
+            # print(
+            #     (
+            #         item.find_element_by_xpath(
+            #             "//*[contains(@id, 'add-to-cart-button')]"
+            #         ).get_attribute("innerText")
+            #     )
+            # )
+            print(
+                "Product Status--",
+                (
+                    item.find_element_by_class_name("add-to-cart-button").get_attribute(
+                        "innerText"
+                    )
+                ),
+            )
+        except:
+            print("not found")
+        try:
+            if (
+                item.find_element_by_xpath(
+                    "//*[contains(@id, 'add-to-cart-button')]"
+                ).get_attribute("innerText")
+            ) == "Add to Cart":
+                sku_value = item.find_element_by_class_name("sku-header")
+                print("siteurl--", sku_value.get_attribute("innerHTML"))
+                print("Product Name--", sku_value.get_attribute("innerText"))
+        except:
+            print("properties not found")
     driver.quit()
 
 
-best_buy_search(search_word="PS5 Console")
+best_buy_search(search_word=args.product)
